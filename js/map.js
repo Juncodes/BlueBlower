7269ea7cddc8fd38112e11343037451e015ad3db
Jun Chai <andychai1992@gmail.com>
2017-02-21 01:27:50 -0800

    added products

--- js/map.js
+++ js/map.js
@@ -14,7 +14,7 @@ function init() {
         scrollwheel: false, //set to true to enable mouse scrolling while inside the map area
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(21.170240, 72.831061), // Surat
+        center: new google.maps.LatLng(34.057655, -117.8215), // Cal poly pomona
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
@@ -214,7 +214,7 @@ function init() {
 
     // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(21.170240, 72.831061),
+        position: new google.maps.LatLng(34.0565, -117.8215),
         map: map,
         title: 'Nileforest',
         icon: 'img/map-marker.png'
